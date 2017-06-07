2b59711d793318470ce500f70a6654399026c467
WizzardCat <sabaku_no_orochi@hotmail.fr>
2017-06-07 16:19:28 +0200

    Second View Creation with intent

--- My_first_apps/app/src/main/java/local/httpandroidstudioprojetct/my_first_apps/MainActivity.java
+++ My_first_apps/app/src/main/java/local/httpandroidstudioprojetct/my_first_apps/MainActivity.java
@@ -3,33 +3,41 @@ package local.httpandroidstudioprojetct.my_first_apps;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         final TextView myText = (TextView) findViewById(R.id.text);
-
 
     Button myButton = (Button) findViewById(R.id.button1);
         myButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
 
-                Toast toast = Toast.makeText(getApplicationContext(), myText.getText(), Toast.LENGTH_SHORT);
+            // Instant non-durable pop-up
+                Toast toast = Toast.makeText(getApplicationContext(), myText.getText(), Toast.LENGTH_LONG);
                 toast.show();
+
+                Intent myIntent = new Intent(MainActivity.this, NextActivity.class);
+                EditText editText = (EditText) findViewById(R.id.text);
+                String message = editText.getText().toString();
+                myIntent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(myIntent);
             }
+
+
         });
     }
+
+
 }
--- My_first_apps/app/src/main/java/local/httpandroidstudioprojetct/my_first_apps/NextActivity.java
+++ My_first_apps/app/src/main/java/local/httpandroidstudioprojetct/my_first_apps/NextActivity.java
@@ -1,18 +1,34 @@
 package local.httpandroidstudioprojetct.my_first_apps;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.widget.TextView;
+
 
 /**
  * Created by Formation on 06/06/2017.
  */
 
 public class NextActivity extends Activity{
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_next);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = (TextView) findViewById(R.id.pseudo);
+        textView.setText(message);
+
+
     }
+
+
 }
